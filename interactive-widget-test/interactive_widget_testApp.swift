3fddcbb7f81efc91c08b820804319b995b3a4bfa
byunghoon <astraljoker@gmail.com>
2023-12-30 18:57:32 -0500

    프로젝트 추가

---
+++ interactive-widget-test/interactive_widget_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactive_widget_testApp.swift
+//  interactive-widget-test
+//
+//  Created by byunghoon on 2023-12-30.
+//
+
+import SwiftUI
+
+@main
+struct interactive_widget_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
