b4a488187624f8f61aa3e755a69a060fd558fe1f
byunghoon <astraljoker@gmail.com>
2023-12-30 21:34:36 -0500

    widget target 추가

---
+++ widget/widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by byunghoon on 2023-12-30.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+    }
+}
